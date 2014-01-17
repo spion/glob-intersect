6ade3491f627b9bb0e479a9ae1a68bc4ad969b3d
Gorgi Kosev <gorgi.kosev@gmail.com>
2014-01-17 14:45:29 +0100

    generalize the code

--- index.js
+++ index.js
@@ -1,45 +1,32 @@
-// Solves the intersection for a single component
+// Solves the intersection in a generalized way
 // Reference: http://stackoverflow.com/questions/2821506/how-do-you-tell-if-two-wildcards-overlap
-function innersect(w1, w2) {
+function innersect(w1, w2, kleene, eq) {
     // both are either empty or contain a wildcard
-    if ((w1 === "" || w1 === "*") &&
-        (w2 === "" || w2 === "*")) return true;
+    if ((w1.length === 0 || w1 === kleene) &&
+        (w2.length === 0 || w2 === kleene)) return true;
     // only one of them is empty, the other is not just a wildcard
-    if (w1 === "" || w2 === "") return false;
-    var c1 = w1[0], c2 = w2[0];
-    var remain1 = w1.slice(1), remain2 = w2.slice(1);
-    // if first letters match and remaining innersect
-    if (c1 === c2 && innersect(remain1, remain2)) return true;
-    // if either is a wildcard and either remaining innersects with the other whole
-    if ((c1 === '*' || c2 === '*') 
-        && (innersect(w1, remain2) || innersect(remain1, w2))) 
-        return true;
-    // else, no match, return false
-    return false;
-}
- 
-// Solves the intersection for paths (arrays of components)
-// and double-kleenes
-function outersect(w1, w2) {
-    // both are either empty or contain a double-kleene
-     if ((w1.length === 0 || w1 === "**") && (w2.length === 0 || w2 === "**")) return true;
-    // one of them is empty, the other one is non-empty, not double-kleene
     if (w1.length === 0 || w2.length === 0) return false;
     var c1 = w1[0], c2 = w2[0];
     var remain1 = w1.slice(1), remain2 = w2.slice(1);
-    // if first items innersect and remaining outersect
-    if (innersect(c1,c2) && outersect(remain1, remain2)) return true;
-    // if either is a wildcard and either remaining outersects with the other
-    if ((c1 === '**' || c2 === '**') 
-        && (outersect(w1, remain2) || outersect(remain1, w2))) 
-        return true;
+    // if first letters match and remaining innersect
+    if (eq(c1, c2) && innersect(remain1, remain2, kleene, eq)) return true;
+    // if either is a wildcard and either remaining innersects with the other whole
+    if ((c1 === kleene || c2 === kleene) && 
+        (innersect(w1, remain2, kleene, eq) || 
+         innersect(remain1, w2, kleene, eq))) return true;
     // else, no match, return false
     return false;
 }
- 
-// Usable export.
+
+function eqChar(c1, c2) { return c1 === c2; }
+function eqSegment(s1, s2) {
+    return innersect(s1, s2, '*', eqChar);
+}
+
+// Usable export, specialized for paths
 function intersect(w1, w2) {
-    return outersect(w1.split('/'), w2.split('/'));
+    return innersect(w1.split('/'), w2.split('/'), '**', eqSegment);
 }
  
 module.exports = intersect;
+
--- test/index.js
+++ test/index.js
@@ -7,7 +7,11 @@ var tests = [
     ["*ends", "begins*", true],
     ["*no", "*intersection", false],
     ["file/dir/*.js", "file/dir/*.cs", false],
-    ["a*db*c", "adbcdbbc", true]
+    ["a*db*c", "adbxdbbc", true],
+    ["a**a", "abcda", true],
+    ["*", "f7*u12", true],
+    ["ax*", "bx", false],
+    ["file/**", "file/dir/*.js", true]
 ];
 
 var t = require('tape');
@@ -16,7 +20,7 @@ var m = require('../index');
 
 t.test("examples", function(t) {
     tests.forEach(function(item) {
-        t.test(item[0] + ', ' + item[1] + ' -> ' + (item[2] ? 'intersect' : 'false'), function(t) {
+        t.test(item[0] + ', ' + item[1] + ' -> ' + (item[2] ? 'yes' : 'no'), function(t) {
             t.equals(m(item[0], item[1]), item[2], 
                      'should ' + item[2] ? '' : 'NOT' + ' intersect');
             t.end();
